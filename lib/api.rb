3b76d59fd7eedf6dd525f71ec7997b014a8f3fea
duruchima <duruchima1@gmail.com>
2020-03-24 09:34:16 -0400

    api file modified increased versatility

--- lib/api.rb
+++ lib/api.rb
@@ -1,19 +1,47 @@
-require 'net/http'
-require 'open-uri'
-require 'json'
+#require 'net/http'
+#require 'open-uri'
+#require 'json'
+
 
 class GetRecipes
     URL = "http://www.recipepuppy.com/api/"
 
     def get_recipes
-        uri = URI.parse(URL)
-        response = Net::HTTP.get_response(uri)
-        response.body
-        
+        uri = URI(URL)
+        response = Net::HTTP.get(uri)
+        JSON.parse(response)
     end
+
+    def url_helper(search_type, ingredient)
+        url = "http://www.recipepuppy.com/api/?#{search_type}=#{ingredient}"
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        JSON.parse(response)
+    end
+
+    def get_recipe_by_ingredient(ingredient)
+        parsed_response = url_helper("i", ingredient)
+    end
+
+
+    def get_recipe_by_name(name)
+        parsed_response = url_helper("q", name)
+        array = parsed_response["results"]
+        new_array = []
+        array.collect do |hash|
+            hash.each do |k, v| if k == "title"
+                new_array << v
+            end
+        end
+        end
+
+        new_array
+    end
+
     
 end
 
-recipes = GetRecipes.new.get_recipes
-#puts recipes
+recipes = GetRecipes.new.get_recipe_by_name("sandwich")
+puts recipes
+binding.pry
 
